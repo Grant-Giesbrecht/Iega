635ae22b9efb1a3aac64d353fe55dbafccac7593
Grant-Giesbrecht <giesbrechtgrant@gmail.com>
2019-03-10 20:01:25 -0700

    Added files from IEGA_Functions XCode project

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  IEGA_Functions
+//
+//  Created by Grant Giesbrecht on 4/16/18.
+//  Copyright © 2018 IEGA. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
